9b5e578f7ae99900944b7823abe56943d70d3db2
Andy Lipton <72267789+Syd4r@users.noreply.github.com>
2024-11-21 19:13:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT`'] = 587
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
